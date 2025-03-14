54bb87991f8a666e40fc747ced669816575178a5
ManoharanShriram <manoharan.hari@relevantz.com>
2025-03-14 17:00:30 +0530

    Commit Mano

---
+++ Manoharan.cs
@@ -0,0 +1,15 @@
+class Team{
+
+
+public static  void Mano1()
+{
+    System.Console.WriteLine("This is Mano's first method");
+}
+
+public static void Mano2()
+{
+    System.Console.WriteLine("This is Mano's second method.");
+
+}
+
+}
