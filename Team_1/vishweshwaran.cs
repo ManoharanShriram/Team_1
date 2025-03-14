0902b859f4f9226624c2eb26c80bcd48e18238eb
Vihweahwaran Elangmani <vishweshwaran.elangamani@Relevantz.com>
2025-03-14 17:02:44 +0530

    the initial commit

---
+++ Team_1/vishweshwaran.cs
@@ -0,0 +1,16 @@
+
+class Vishweshwarn-Elangamani{
+
+
+public static  void Vishwa1()
+{
+    System.Console.WriteLine("This is vishwa first method");
+}
+
+public static void Vishwa2()
+{
+    System.Console.WriteLine("This is vishwa second method");
+
+}
+
+}
